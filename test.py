eed57f59ef1893ba9ab49d00cacebe1575467923
ujjwalm9895 <ujjwalm9895@gmail.com>
2025-06-29 23:08:20 +0530

    ci-cd

--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-def generate_image(prompt):
-    import requests
-    import os
+import base64
 
-    api_key = os.getenv("RUNWARE_API_KEY")
-    url = "https://api.runware.ai/v1/generate"
-
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
-    }
-
-    payload = {
-        "prompt": prompt,
-        "width": 512,
-        "height": 512,
-        "steps": 25,
-        "guidance_scale": 7
-    }
-
-    try:
-        response = requests.post(url, headers=headers, json=payload)
-        response.raise_for_status()
-        return response.json().get("image_url")
-    except Exception as e:
-        print("❌ Runware error:", e)
-        print(response.text)
-        return None
+with open("google-credentials.json", "rb") as f_in, open("key.b64.txt", "wb") as f_out:
+    f_out.write(base64.b64encode(f_in.read()))
