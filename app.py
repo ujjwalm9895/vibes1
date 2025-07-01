3bb64be22e4df975cbf3e767f41757eb22c5419b
ujjwalm9895 <ujjwalm9895@gmail.com>
2025-07-01 10:09:52 +0530

    run

--- app.py
+++ app.py
@@ -324,5 +324,5 @@ def handle_socket(ws):
                 except Exception as e:
                     print("❌ Call end/save error:", e)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
